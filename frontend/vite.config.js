95971e9fce4838817c333dca08e8eea62f81a6c1
Punna Vardhan <punna.vardhan@gmail.com>
2025-12-06 14:57:17 +0530

    update cors and buidl config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/',
+  root: './',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
   server: {
     port: 3000,
     open: true,
